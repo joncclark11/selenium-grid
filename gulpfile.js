831150af18a8e777dddf25c831c084fc94176283
Jon Clark <jon.c.clark11@gmail.com>
2016-12-15 15:14:38 -0500

    init: Added gulpfile with basic tasks and package.json with dependencies and test script

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var selenium = require('selenium-standalone');
+var mocha = require('gulp-mocha');
+
+gulp.task('serve:test', function(done) {
+    browserSync({
+        logLevel: 'silent',
+        notify: false,
+        open: false,
+        port: 9000,
+        server: {
+            baseDir: ['test']
+        },
+        ui: false
+    }, done);
+});
+
+grunt.task('selenium', function(done) {
+    selenium.install({
+        logger: function(message) {}
+    }, function(err) {
+        if(err) return done(err);
+        selenium.child = child;
+        done();
+    });
+});
+
+gulp.task('integration', ['serve:test', 'selenium'], function() {
+    return gulp.src('test/spec/**/*.js', {read: false})
+        .pipe(mocha());
+});
+
+gulp.task('test', ['integration'], function() {
+    selenium.child.kill();
+    browserSync.exit();
+});
